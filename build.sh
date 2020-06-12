ee51826ab8ed59b981087c83e8fd575546353f9b
Ubuntu <ubuntu@ip-172-31-20-189.eu-central-1.compute.internal>
2020-06-12 15:36:03 +0000

    ACCESS_REP_TOKEN check added

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 export AIRT_DOCKER=registry.gitlab.com/airt.ai/airt-docker-dask-tf2
 
-echo Building $AIRT_DOCKER
+if test -z "ACCESS_REP_TOKEN"
+then
+	echo ERROR: ACCESS_REP_TOKEN must be defined, exiting
+	return -1
+else
+	echo Building $AIRT_DOCKER
 
-docker build --build-arg ACCESS_REP_TOKEN -t $AIRT_DOCKER:`date -u +%Y.%m.%d-%H.%M.%S` -t $AIRT_DOCKER:latest .
+	docker build --build-arg ACCESS_REP_TOKEN -t $AIRT_DOCKER:`date -u +%Y.%m.%d-%H.%M.%S` -t $AIRT_DOCKER:latest .
+fi
+
